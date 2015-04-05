c27b1678c67a3a59e505726a0afb8606804c0b09
aankit <aankit.patel@gmail.com>
2015-04-05 15:08:41 -0400

    uwsgi setup

--- runserver.py
+++ runserver.py
@@ -2,11 +2,14 @@ import os
 from application import app
 from application import scheduler
 
-def runserver():
+# def runserver():
+# 	scheduler.start()
+# 	print 'scheduler started'
+# 	port = int(os.environ.get('PORT', 3000))
+# 	app.run('0.0.0.0', port=port)
+
+if __name__ == '__main__':
 	scheduler.start()
 	print 'scheduler started'
 	port = int(os.environ.get('PORT', 3000))
 	app.run('0.0.0.0', port=port)
-
-if __name__ == '__main__':
-	runserver()
