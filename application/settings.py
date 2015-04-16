fa2e80e7529f3fbcf8000ee4cd8f3f6e9aad7057
aankit <aankit.patel@gmail.com>
2015-04-16 13:16:33 -0400

    giving up on uwsgi

--- application/settings.py
+++ application/settings.py
@@ -1,4 +1,4 @@
-DEBUG = True
+DEBUG = False
 SECRET_KEY = 'newkey'  # make sure to change this
 SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://aha:aha@/aha'  # 'sqlite:///test.db'
 WTF_CSRF_ENABLED = True
