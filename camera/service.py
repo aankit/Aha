546ca7bd0ffe8f24bb444138830aab31068728a8
aankit <aankit.patel@gmail.com>
2015-04-18 23:04:46 -0400

    camera refresh service

--- camera/service.py
+++ camera/service.py
@@ -5,7 +5,7 @@ from camera import control
 refresh_state = control.recording_refresh()
 
 if refresh_state is None:
-    filename = contorl.record_on()
+    filename = control.record_on()
 elif refresh_state:
     filename = control.refresh_state
 
