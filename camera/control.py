64886fd36c8b274b0e64d78052416d56f13d45cf
aankit <aankit.patel@gmail.com>
2015-04-18 21:05:20 -0400

    separating cam function from flask app

--- camera/control.py
+++ camera/control.py
@@ -25,8 +25,8 @@ def service_state():
 def record_on():
     sh.touch(settings.ONOFF_PATH + '/start_record')
     current_file = os.listdir(settings.CURRENT_RECORDING_PATH)
-    # while len(current_file) == 0:
-    #     current_file = os.listdir(settings.CURRENT_RECORDING_PATH)
+    while len(current_file) == 0:
+        current_file = os.listdir(settings.CURRENT_RECORDING_PATH)
     return current_file[0]  # only one file can exist here at a time
 
 
