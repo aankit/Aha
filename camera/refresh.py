dafc71b396f904725af329a085687dc70713fcaf
aankit <aankit.patel@gmail.com>
2015-04-18 20:48:19 -0400

    separating cam function from flask app

--- camera/refresh.py
+++ camera/refresh.py
@@ -1,10 +1,11 @@
 from camera import control
 
-current_file = control.record_state()
-if current_file:
-    control.record_off()
-    control.record_on()
-else:
-    print "nothing recording"
+def refresh_recording():
+    current_file = control.record_state()
+    if current_file:
+        control.record_off()
+        control.record_on()
+    else:
+        print "nothing recording"
 
 #TODO maybe save filename in database here
