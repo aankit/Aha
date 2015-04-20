a9cb2b59bca3cab4cb610e6bce6f84c30f139b5c
aankit <aankit.patel@gmail.com>
2015-04-20 13:04:23 -0400

    adding web side cam controls

--- application/controllers.py
+++ application/controllers.py
@@ -1,4 +1,4 @@
-from flask import request, session, redirect, flash
+from flask import request, session, redirect, flash, g
 from flask import render_template, url_for
 from application.models import *
 from application import app, api_manager
