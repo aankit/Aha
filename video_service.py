c597ef4b61745cc7df7159d576104ec12c667df8
aankit <aankit.patel@gmail.com>
2015-04-25 02:43:47 -0400

    video concat service throwing ffmpeg error, vague but promising...nerdy coincidence

--- video_service.py
+++ video_service.py
@@ -2,27 +2,31 @@
 
 from sh import ffmpeg
 import os
-from datetime import datetime
+from datetime import datetime, timedelta
 from application.models import Schedule, Marker
 from application import media_dir
 
 #let's get a list of files in each media subdirectory, if there is more than one, combine them
 #using the vidlist.txt
 
-string_date = datetime.strftime(datetime.now(), '%Y_%m_%d')
-print string_date
+today_string = datetime.strftime(datetime.now(), '%Y_%m_%d')
+yest_string = datetime.strftime(datetime.now() - timedelta(days=1), '%Y_%m_%d')
+print today_string
+print yest_string
+
 for model in [Schedule, Marker]:
     for result in model.query.all():
         print result
         try:
-            investigation_id = str(model.investigation.id)
+            investigation_id = str(result.investigation.id)
         except:
             investigation_id = "markers"  # markers don't initially have an investigation id
 
-        media_path = media_dir + '/' + investigation_id + '/' + str(result.id) + '/' + string_date
+        media_path = media_dir + '/' + investigation_id + '/' + str(result.id) + '/' + yest_string
         print media_path
         if os.path.isdir(media_path):
             vid_files = os.listdir(media_path)
             print vid_files
             if len(vid_files) > 1 and os.path.isfile(media_path + '/' + 'vidlist.txt'):
-                ffmpeg('-f', 'concat', '-i', 'vidlist.txt')
+                print "getting ready to concat with ffmpeg"
+                ffmpeg('-f', 'concat', '-i', 'vidlist.txt', '-c:v', 'copy', '-c:a', 'copy', '-bsf:a', 'aac_adtstoasc', 'final.mp4')
