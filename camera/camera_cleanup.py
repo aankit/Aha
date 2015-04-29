2a7fcfe9894f66985dcba75ee7ebf020e8f526ee
aankit <aankit.patel@gmail.com>
2015-04-29 00:32:45 -0400

    trying separating RAM cleanup

---
+++ camera/camera_cleanup.py
@@ -0,0 +1,8 @@
+from camera import control, settings
+from datetime import datetime
+import os
+from application import media_dir
+
+for filepath in control.get_all_recordings():
+    filename = filepath[len(settings.ARCHIVE_RECORDING_PATH):-3]
+    print filename
