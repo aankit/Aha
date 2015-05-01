148244f5bc3e8cce48c4b0652d12a05692f2dbb2
aankit <aankit.patel@gmail.com>
2015-05-01 04:47:34 -0400

    waiting for refresh before sending old file to staging

--- stage_video.py
+++ stage_video.py
@@ -1,9 +1,11 @@
 import aha
-from glob import glob
+
 refresh_state = aha.refresh_camera()
 if refresh_state:
     last_recording = aha.get_last_recorded()
+    while last_recording is None:
+        last_recording = aha.get_last_recorded()
     aha.stage_file(last_recording)
-else if aha.get_all_recorded():
+elif aha.get_all_recorded():
     for recording in aha.get_all_recorded():
         aha.stage_file(recording)
