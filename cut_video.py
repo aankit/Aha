c98a689e1ee619cc921b332543e1836f12958645
aankit <aankit.patel@gmail.com>
2015-05-01 05:15:33 -0400

    spelling

--- cut_video.py
+++ cut_video.py
@@ -7,6 +7,6 @@ for filename in aha.get_staged_files():
         media_path = aha.get_media_path(filename, match)
         start, duration = aha.get_relative_cut(filename, match)
         aha.cut_file(filename, media_path, start, duration)
-    if aha.check_purge_status(filename):
+    if aha.check_purge_state(filename):
         os.remove(filename)
         print "removed %s" % (filename)
