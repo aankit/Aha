c8ff88d7b543e09b8e422b7eacc44237503a8243
aankit <aankit.patel@gmail.com>
2015-05-02 16:21:11 -0400

    concatenating successfully, now its time to clean media paths

--- build_video.py
+++ build_video.py
@@ -3,8 +3,6 @@ import aha
 while True:
     for media_path, db_duration in aha.get_media_paths():
         gap_total, num_files = aha.check_consecutive(media_path)
-        video_duration = aha.check_duration(media_path)
-        duration_diff = db_duration - video_duration
         print "this is the gap total: %d" % (gap_total)
         if gap_total < 4 * num_files:
             print "concatenating %s" % (media_path)
@@ -14,9 +12,10 @@ while True:
                 aha.make_thumbnail(media_path)
         else:
             print gap_total
-
-        # if duration_diff <= 10:
-        #     print "cleaning %s" % (media_path)
-        #     aha.clean_build_media(media_path)
-        # else:
-        #     print duration_diff
+        video_duration = aha.check_duration(media_path)
+        duration_diff = db_duration - video_duration
+        if duration_diff <= 10:
+            print "cleaning %s" % (media_path)
+            aha.clean_build_media(media_path)
+        else:
+            print duration_diff
