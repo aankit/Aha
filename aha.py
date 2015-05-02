883747509b5e7021bd96c11c84d38fdffdc675dc
aankit <aankit.patel@gmail.com>
2015-05-01 23:37:24 -0400

    adding function to check consecutive videos

--- aha.py
+++ aha.py
@@ -109,6 +109,8 @@ def get_media_path(filename, match):
 
 def check_consecutive(sorted_filenames):
     for index in range(1, len(sorted_filenames)):
+        print sorted_filenames[index]
+        print sorted_filenames[index-1]
         first_starttime_obj, first_endtime_obj = get_file_timestamps(sorted_filenames[index])
         second_starttime_obj, second_endtime_obj = get_file_timestamps(sorted_filenames[index-1])
         time_diff = second_endtime_obj - first_starttime_obj
