178ee9b01c5c90280f6adda06fd6df19abd4783d
aankit <aankit.patel@gmail.com>
2015-05-02 15:11:05 -0400

    tested aha scripts, now trying to run as services

--- build_video.py
+++ build_video.py
@@ -1,20 +1,19 @@
 import aha
 
-for media_path, db_duration in aha.get_media_paths():
-    gaps = aha.check_consecutive(media_path)
-    video_duration = aha.check_duration(media_path)
-    duration_diff = db_duration - video_duration
-    print gaps
-    if gaps < 15:
-        aha.sort_concat_file(media_path)
-        aha.concatenate(media_path)
-        aha.make_thumbnail(media_path)
-    else:
-        print gaps
-    print duration_diff
-    if duration_diff <= 10:
-        print "cleaning"
-        aha.clean_build_media(media_path)
-    else:
-        print "here"
-        print duration_diff
+while True:
+    for media_path, db_duration in aha.get_media_paths():
+        gaps = aha.check_consecutive(media_path)
+        video_duration = aha.check_duration(media_path)
+        duration_diff = db_duration - video_duration
+        if gaps < 15:
+            print "concatenating %s" % (media_path)
+            aha.sort_concat_file(media_path)
+            aha.concatenate(media_path)
+            aha.make_thumbnail(media_path)
+        else:
+            print gaps
+        if duration_diff <= 10:
+            print "cleaning %s" % (media_path)
+            aha.clean_build_media(media_path)
+        else:
+            print duration_diff
--- cut_video.py
+++ cut_video.py
@@ -1,13 +1,14 @@
 import os
 import aha
 
-for filename in aha.get_staged_files():
-    matches = aha.get_db_matches(filename)
-    for match in matches:
-        media_path = aha.make_media_path(filename, match)
-        media_filename = media_path + '/' + aha.remove_path(filename)
-        if not os.path.isfile(media_filename):
-            aha.cut_file(media_path, filename, match)
-    if aha.check_purge_state(filename) is True:
-        os.remove(filename)
-        print "removed %s" % (filename)
+while True:
+    for filename in aha.get_staged_files():
+        matches = aha.get_db_matches(filename)
+        for match in matches:
+            media_path = aha.make_media_path(filename, match)
+            media_filename = media_path + '/' + aha.remove_path(filename)
+            if not os.path.isfile(media_filename):
+                aha.cut_file(media_path, filename, match)
+        if aha.check_purge_state(filename) is True:
+            os.remove(filename)
+            print "removed %s" % (filename)
