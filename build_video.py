2e03146ecc84141c2a5531afb85a78c4e51bf2ae
aankit <aankit.patel@gmail.com>
2015-05-02 12:32:44 -0400

    setting up build video for test

--- build_video.py
+++ build_video.py
@@ -2,13 +2,16 @@ import aha
 
 for media_path, db_duration in aha.get_media_paths():
     gaps = aha.check_consecutive(media_path)
-    print gaps
+
     video_duration = aha.check_duration(media_path)
     duration_diff = db_duration - video_duration
-    print duration_diff
     if gaps < 15:
         aha.sort_concat_file(media_path)
         aha.concatenate(media_path)
         aha.make_thumbnail(media_path)
+    else:
+        print gaps
     if duration_diff <= 10:
         aha.clean_build_media(media_path)
+    else:
+        print duration_diff
