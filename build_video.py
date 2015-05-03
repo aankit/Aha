0a44cc847fa428de8f50a56eb985f1eee120adeb
aankit <aankit.patel@gmail.com>
2015-05-02 23:58:23 -0400

    remove commenting

--- build_video.py
+++ build_video.py
@@ -7,12 +7,12 @@ while True:
         video_duration = aha.check_duration(media_path)
         duration_diff = db_duration - video_duration
         if aha.build_ready(media_path) and aha.new_file_exists(media_path):
-            print "concatenating %s" % (media_path)
+            # print "concatenating %s" % (media_path)
             aha.sort_concat_file(media_path)
             aha.concatenate(media_path)
             aha.make_thumbnail(media_path)
         if duration_diff <= 15:
-            print "cleaning %s" % (media_path)
+            # print "cleaning %s" % (media_path)
             aha.clean_build_media(media_path)
         # else:
         #     print duration_diff
