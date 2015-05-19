97c8f1b49313621a88a8266bd89a427e93954f5e
aankit <aankit.patel@gmail.com>
2015-05-19 14:37:40 -0400

    fixing filename issue

--- aha_demo.py
+++ aha_demo.py
@@ -8,8 +8,8 @@ while True:
         print matches
         for match in matches:
             demo_path = '/var/www/Aha/media/demo'
-            demo_filename = demo_path + '/' + aha.remove_path(filename)
-            aha.cut_file(demo_path, demo_filename, match)
+            # demo_filename = demo_path + '/' + aha.remove_path(filename)
+            aha.cut_file(demo_path, filename, match)
             aha.make_thumbnail(demo_path)
             aha.transcode(demo_path)
         os.remove(filename)
