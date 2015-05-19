a294cda729ec0198dcbc7493f2943f896bb6d1f5
aankit <aankit.patel@gmail.com>
2015-05-19 13:58:26 -0400

    setting up show demo script

---
+++ aha_demo.py
@@ -0,0 +1,13 @@
+import aha
+import os
+
+while True:
+    for filename in aha.get_staged_files():
+        matches = aha.get_db_matches(filename)
+        for match in matches:
+            demo_path = '/var/www/Aha/media/demo'
+            demo_filename = 'demo.ts'
+            aha.cut_file(demo_path, demo_filename, match)
+            aha.make_thumbnail(demo_path)
+            aha.transcode(demo_path)
+            os.remove(filename)
